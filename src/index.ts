57455a7299aa1578f207f7d73fce1cd37cce4332
Sergio Cano <serdeliverance@gmail.com>
2020-09-28 08:53:33 -0300

    User model with get, set methods and userProps interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log('getting started')
+import { User } from './models/User'
+
+let user = new User({ name: 'pepe', age: 20 })
+
+user.set({ name: 'pepe2', age: 25 })
+
+console.log(user.get('name'))
