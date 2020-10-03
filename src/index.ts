c660d0cffeafcd2bba69cc4665204cd1eaa6aa21
Sergio Cano <serdeliverance@gmail.com>
2020-10-03 09:48:50 -0300

    refactor user class using composition and extracting event, sync and attributes into classes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User'
 
 let user = new User({ name: 'NEW RECORD', age: 16 })
 
-user.set({ name: 'NEW NAME', age: 33 })
-
-user.save()
+user.events.on('change', () => {
+  console.log('change')
+})
---
+++ src/models/Attributes.ts
@@ -0,0 +1,11 @@
+export class Attributes {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName]
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update)
+  }
+}
